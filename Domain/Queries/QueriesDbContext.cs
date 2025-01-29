e7e5f9e85fe47b451349d9570d5bf9c86ea5d573
guilherme freitas <guiFreitas2612@gmail.com>
2025-01-28 22:26:44 -0300

    3

---
+++ Domain/Queries/QueriesDbContext.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+
+namespace EducaPlayAPI.Domain.Queries
+{
+    public class QueriesDbContext(DbContextOptions<QueriesDbContext> options) : DbContext(options)
+    {
+        protected override void OnModelCreating(ModelBuilder builder) { }
+
+        public async Task<(bool CanConnect, string ErrorMessage)> TryConnectionAsync()
+        {
+            try
+            {
+                var canConnect = await this.Database.CanConnectAsync();
+                if (canConnect)
+                    return (true, null);
+                await this.Database.OpenConnectionAsync();
+                this.Database.CloseConnection();
+                return (false, null);
+            }
+            catch (Exception ex)
+            {
+                return (false, ex.Message);
+            }
+        }
+    }
+}
